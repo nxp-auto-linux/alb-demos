d91a884c65208b7c7f003fc4b3d838f4dab76a35
Bogdan Hamciuc <bogdan.hamciuc@nxp.com>
2016-09-05 17:40:26 +0300

    Move install, clean targets to common.mk
    Simplify leaf Makefiles by moving every remaining 'standard' target into
    common.mk. A number of environment variables have been documented that
    Makefiles need to define or override.
    
    Signed-off-by: Bogdan Hamciuc <bogdan.hamciuc@nxp.com>

--- common.mk
+++ common.mk
@@ -3,6 +3,22 @@
 #
 # SPDX-License-Identifier:     BSD-3-Clause
 #
+# Convenience definitions of variables and targets for the make subsystems.
+# This file also defines environment variables for Yocto.
+#
+# The following variables must be defined by the Makefiles in the leaves of the
+# dependency tree:
+#  - $(EXE)		- Name of the final executable
+#  - $(OBJ)		- List of object files
+#  - $(DEPS)		- Optional, list of additional build dependencies
+#  - $(INSTALLEXTRA)	- Optional, files to be installed in addition to $(EXE)
+#  - $(CLEANEXTRA)	- Optional, files to be cleaned in addition to $(EXE) and $(OBJ)
+#
+# Standard variables such as $(CFLAGS) and $(LDFLAGS) can also be overridden.
+#
+# Makefiles that need these definitions should include this file
+# *on the last line*, to make sure all the locally-defined variables are
+# properly expanded.
 
 .PHONY: all install clean
 
@@ -21,10 +37,6 @@ CFLAGS += --sysroot=$(SYSROOT)
 LDFLAGS += --sysroot=$(SYSROOT)
 endif
 
-# $(EXE), $(OBJ) and (optionally) $(DEPS) must be defined by the Makefiles
-# from the leaves of the dependency tree. Standard variables such as $CFLAGS
-# and $(LDFLAGS) can/should also be overridden.
-
 all: $(EXE)
 
 $(EXE): $(OBJ)
@@ -33,6 +45,24 @@ $(EXE): $(OBJ)
 # Convenience rule to make individual objects
 %.o: %.c $(DEPS)
 	$(CC) -o $@ -c $< $(CFLAGS)
+
+# $(INSTALLDIR) should have been prepared by the parent Makefile; do nothing
+# in case it hasn't, to avoid silly errors in case 'make' is executed directly
+# from the lower-dir level.
+ifeq ($(INSTALLDIR),)
+install:
+	$(info >> INSTALLDIR not set, skipping install.)
+else
+install: all
+# Livin' on the edge: if $(INSTALLDIR) is a file, remove it; otherwise,
+# we'll just add to a pre-existing folder.
+	if [ -f $(INSTALLDIR) ]; then rm -f $(INSTALLDIR); fi
+	mkdir -p $(INSTALLDIR)
+	install -v $(EXE) $(INSTALLEXTRA) $(INSTALLDIR)
+endif
+
+clean:
+	rm -rf $(OBJ) $(EXE) $(CLEANEXTRA)
 
 # Force default target. Individual Makefiles can override this as needed.
 .DEFAULT_GOAL = all
