2d07064d6acd5fc30309b6718b902abf3fa3d62c
Bogdan Hamciuc <bogdan.hamciuc@nxp.com>
2016-09-05 17:13:47 +0300

    Split 'all' target and move to common.mk
    Split all: target to explicitly make objects and executable. Move the
    resulting rules into common.mk which is now included at the end of the
    Makefiles at the leaves of the dependency tree (to avoid things like
    secondary expansion).
    
    For now, multicore/Makefile is only barely touched, to preserve its
    functionality.
    
    Signed-off-by: Bogdan Hamciuc <bogdan.hamciuc@nxp.com>

--- common.mk
+++ common.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier:     BSD-3-Clause
 #
 
+.PHONY: all install clean
+
 ifeq ($(CC)_$(CROSS_COMPILE),_)
 $(warning CROSS_COMPILE is not set!)
 endif
@@ -12,13 +14,25 @@ ifneq ($(CROSS_COMPILE),)
 CC := $(CROSS_COMPILE)gcc
 endif
 
-CFLAGS ?= -Wall
+CFLAGS += -Wall
 
 ifneq ($(SYSROOT),)
 CFLAGS += --sysroot=$(SYSROOT)
 LDFLAGS += --sysroot=$(SYSROOT)
 endif
 
+# $(EXE), $(OBJ) and (optionally) $(DEPS) must be defined by the Makefiles
+# from the leaves of the dependency tree. Standard variables such as $CFLAGS
+# and $(LDFLAGS) can/should also be overridden.
+
+all: $(EXE)
+
+$(EXE): $(OBJ)
+	$(CC) -o $(EXE) $^ $(LDFLAGS)
+
 # Convenience rule to make individual objects
 %.o: %.c $(DEPS)
-	$(CC) -c -o $@ $< $(CFLAGS)
+	$(CC) -o $@ -c $< $(CFLAGS)
+
+# Force default target. Individual Makefiles can override this as needed.
+.DEFAULT_GOAL = all
