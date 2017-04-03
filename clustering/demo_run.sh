8fb886ec0d6b8557e77d5cdc6104e9e84cc5dd69
Nica Dan <dan.nica@nxp.com>
2017-04-03 15:34:39 +0300

    clustering: Call demo.pg using gnuplot
    Fix an issue where demo.pg would complain about
    incorrect syntax. Problem is solved by preceeding
    the call to demo.pg with gnuplot
    
    Issue: ALB-777
    Signed-off-by: Nica Dan <dan.nica@nxp.com>

--- clustering/demo_run.sh
+++ clustering/demo_run.sh
@@ -1,5 +1,5 @@
 # Copyright 2017 NXP
-./demo.pg > c.png
+gnuplot ./demo.pg > c.png
 convert.im6 white.png result_so_far.ppm -gravity SouthEast -composite result_so_far2.ppm
 convert.im6 c.png result_so_far2.ppm +append result.png
 convert.im6 -type truecolor -format bmp -define bmp:format=bmp3 -transpose -rotate 270 result.png result.bmp
